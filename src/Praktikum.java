4baff624d7779a8a857d478136ea60bd9ea0cd51
Vladimir Nazarenko <vnazaren@yandex.ru>
2023-08-18 10:39:34 +0300

    Task is done.

---
+++ src/Praktikum.java
@@ -0,0 +1,29 @@
+/**
+ * @author Vladimir Nazarenko
+ */
+
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Введите год:");
+            int year = scanner.nextInt();
+
+            if (isLeapYear(year))
+                System.out.println("12.09." + year);
+            else
+                System.out.println("13.09." + year);
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else
+            return year % 4 == 0;
+    }
+}
